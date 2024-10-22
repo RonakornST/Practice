b36e949faf9290c34c382ed80283f9df339f4164
RonakornST <119744243+RonakornST@users.noreply.github.com>
2024-10-22 17:05:29 +0700

    add basic3 & .gitignore

---
+++ cpp_pratice/basic3Loop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     int i = 0;
+//     // i 0 -10
+//     for(int i = 0; i <= 10;i++){
+//         cout<<i<<endl;
+//     }
+   
+// }
+
+int  main(){
+    int i = 0;
+    // i 0 -10
+    while(i != 10){
+        cout<<i<<endl;
+        i++;
+    }
+}
